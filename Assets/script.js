393b3aae4912b0cf33b3d5aa3c0f9fd984d2ec5b
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-06-09 17:54:14 -0700

    (HTML) added save-button and saved-text id's for js querys; (JS) added btn functionality and saved user input into local storage for 'past' class

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,27 @@
+// QUERY SELECTORS
+
+var currentDay = document.querySelector("#currentDay");
+var saveBtn = document.querySelector("#save-button");
+var savedText = document.querySelector("#saved-text");
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
+  // TODO: Add code to display the current date in the header of the page.
+  function displayCurrentDay() {
+    currentDay.textContent = dayjs().format("dddd, MMMM D, YYYY");
+  }
+  displayCurrentDay();
+  //
+  //
+  //
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
+  saveBtn.addEventListener("click", clickSave);
+  function clickSave() {
+    var scheduledEvent = savedText.value;
+    localStorage.setItem("scheduledEvent", scheduledEvent);
+    // localStorage.setItem("");
+  }
+  // }
   //
   // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
   //
@@ -18,12 +39,6 @@ $(function () {
   //
   //
   //
-  // TODO: Add code to display the current date in the header of the page.
-  function displayCurrentDay() {
-    var currentDay = document.querySelector("#currentDay");
-    currentDay.textContent = dayjs().format("dddd, MMMM D, YYYY");
-  }
-  displayCurrentDay();
   //
   //
   //
