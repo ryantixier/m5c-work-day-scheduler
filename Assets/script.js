7b77c201995a095b71348db3d00b209f3984a61e
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-06-22 12:57:29 -0600

    (/STARTER-CODE) folder represents project starter code to be referred to in the README.md; (INDEX.HTML) added hour blocks for full schedule; (SCRIPT.JS) refactored localStorage to save/present scheduled events in corresponding timeblock.

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,11 @@
 // QUERY SELECTORS
-
 var currentDay = document.querySelector("#currentDay");
 var saveBtn = document.getElementById("save-button");
-var savedText = document.getElementById("saved-text");
 // var timeFrame = document.getElementById("hour-x");
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
-  // TODO: Add code to display the current date in the header of the page.
+  //
   function displayCurrentDay() {
     currentDay.textContent = dayjs().format("dddd, MMMM D, YYYY");
   }
@@ -18,33 +16,58 @@ $(function () {
   //
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
 
-  var scheduledEventArray = [];
-  saveBtn.addEventListener("click", clickSave);
-  function clickSave() {
-    var scheduledEvent = savedText.value.trim();
-    scheduledEventArray.push(scheduledEvent);
-    localStorage.setItem("scheduledEvent", JSON.stringify(scheduledEvent));
-    console.log(scheduledEventArray);
+  // var scheduledEventArray = [];
+  $(".saveBtn").on("click", clickSave);
+  function clickSave(event) {
+    event.preventDefault();
+    var scheduledEvent = $(event.target).siblings()[1].value.trim();
+    // TO DO: add scheduledEvent to array
+    var key = $(event.target).parent().attr("id");
+    var storedEvent = localStorage.getItem("scheduledEvent");
+    if (storedEvent) {
+      storedEvent = JSON.parse(storedEvent);
+    } else {
+      storedEvent = {};
+    }
+    storedEvent[key] = scheduledEvent;
+
+    localStorage.setItem("scheduledEvent", JSON.stringify(storedEvent));
   }
   //
-  // WORK ON CODE BELOW
   //
   //
   function renderScheduledEvents() {
-    var storedEvent = JSON.parse(localStorage.getItem("scheduledEvent"));
-    savedText.textContent = storedEvent;
-    // console.log(storedEvent);
-    if (storedEvent !== null) {
-      storedEvent.textContent = scheduledEventArray.length;
-      console.log(scheduledEventArray.length);
-      for (var i = 0; i < scheduledEventArray.length; i++) {
-        var storedEvent = scheduledEventArray[i];
-      }
+    var storedEvent = localStorage.getItem("scheduledEvent");
+    if (storedEvent) {
+      storedEvent = JSON.parse(storedEvent);
     } else {
-      return;
+      storedEvent = {};
     }
+    Object.entries(storedEvent).forEach(([key, value]) => {
+      var block = $(`#${key}`).children()[1];
+      block.textContent = value;
+      console.log(block);
+    });
+
+    //
+    // separate forEach to add/remove classes to make the times color-change
+    //      iterate through blocks
+    //        array of id's? ["hour-9", "hour-10", ..., "hour-17"]
+    //        $(`#${key}`)
+    //      add past/present/future based on current time of day
+    //      remove past/present/future from HTML
+    //
+
+    // savedText.textContent = storedEvent;
+    // console.log(storedEvent);
+    // storedEvent.textContent = scheduledEventArray.length;
+    // console.log(scheduledEventArray.length);
+    // for (var i = 0; i < scheduledEventArray.length; i++) {
+    // var storedEvent = scheduledEventArray[i];
+    // }
   }
   renderScheduledEvents();
+
   //
   // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
   //
@@ -69,3 +92,14 @@ $(function () {
   //
   //
 });
+
+// TO-DO'S
+// TO-DO'S
+// TO-DO'S
+//
+// connect each save button to corresponding time block
+// ... using 'this'
+//
+// create function so color-coding dynamically corresponds to time of day
+// ... QUESTION: edit CSS at all?
+//
